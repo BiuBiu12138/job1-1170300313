345e7f77d3cd86192c7835f5f8cae7b1b5d84642
BiuBiu12138 <46275913+BiuBiu12138@users.noreply.github.com>
2019-07-06 12:14:53 +0800

    hello1170300220
    fgh

---
+++ helloA3_1170300220.c
@@ -0,0 +1,16 @@
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+//This is my A3  .being pulled request by hit-1170300220
+
+int main()
+{
+    printf("Hello world! I'm TianYi Wang This is my homework A3.\n");
+    printf("This is another request pulled by 1170300220\n");
+    for(int i = 0;i<10;i++){
+        printf("Hello my friend\n");
+    }
+    return 0;
+}
