7f3ee891e67b69640ee6a7087351900025452670
BiuBiu12138 <46275913+BiuBiu12138@users.noreply.github.com>
2019-07-06 12:41:49 +0800

    Create hellofrom1170300220.c

---
+++ hellofrom1170300220.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+{
+    printf("Hello world!\n");
+    
+    for(int i = 0;i<10;i++){
+        printf("Hello my love\n");
+    }
+    return 0;
+}
