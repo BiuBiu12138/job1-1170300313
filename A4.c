316a39af2f9cc67b8692773c01f538a97fe6a95b
BiuBiu12138 <46275913+BiuBiu12138@users.noreply.github.com>
2019-07-06 12:43:26 +0800

    Create A4.c

---
+++ A4.c
@@ -0,0 +1,15 @@
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main()
+{
+    printf("Hello world! A4\n");
+    for(int i = 0;i<10;i++){
+        printf("Hello my friend\n");
+    }
+    return 0;
+}
